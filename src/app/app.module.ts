bde49b30868a1b2bd392ae32965eead00042da2d
Jeremy <jeremymcote@gmail.com>
2021-06-19 20:38:52 -0400

    Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { NgLocalization } from "@angular/common";
+
+import { CarouselModule} from "ngx-bootstrap/carousel";
+
 import { CollapseModule} from "ngx-bootstrap/collapse";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +14,7 @@ import { LandingComponent } from './landing/landing.component';
 import { NavbarComponent} from "./shared/navbar/navbar.component";
 import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { FooterComponent} from "./shared/footer/footer.component";
 
 @NgModule({
   declarations: [
@@ -18,12 +23,15 @@ import { ProjectsComponent } from './projects/projects.component';
     NavbarComponent,
     SkillsComponent,
     ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CollapseModule,
     NgbModule,
+    CarouselModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
