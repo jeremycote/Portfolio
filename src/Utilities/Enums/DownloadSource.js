4fd5768ac8cc9a823914a9b91a6893c007c0303e
Jeremy Cote <jeremymcote@gmail.com>
2022-04-23 17:30:00 -0400

    DownloadSource.js

--- src/Utilities/Enums/DownloadSource.js
+++ src/Utilities/Enums/DownloadSource.js
@@ -2,7 +2,7 @@ class DownloadSource {
     static AppleStoreMac = new DownloadSource("images/DownloadMacEn.svg");
     static AppleStore = new DownloadSource("images/DownloadiOSEn.svg");
     static GoogleStore = new DownloadSource("images/DownloadGoogle.png");
-    
+
     constructor(url) {
         this.url = url;
     }
